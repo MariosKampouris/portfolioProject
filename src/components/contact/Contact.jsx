f5bbc7b68c242d2cdca7d979bf681c788db82509
MariosKampouris <marioswow20@gmail.com>
2023-01-23 03:30:22 +0200

    Started Implementing Contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/md'
+import { BsWhatsapp } from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>marioswow20@gmail.com</h5>
+            <a href='mailto:marioswow20@gmail.com' target='_blank' rel='noreferrer'>Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <MdOutlineEmail/>
+            <h4>Messenger</h4>
+            <h5>dummymessenger</h5>
+            <a href='mailto:dummyegator@gmail.com' target='_blank' rel='noreferrer'>Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <MdOutlineEmail/>
+            <h4>WhatsApp</h4>
+            <h5>+153113132</h5>
+            <a href='mailto:dummyegator@gmail.com' target='_blank' rel='noreferrer'>Send a Message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form action=''>
+          <input type='text' name='name' placeholder='Your Full Name' required/>
+          <input type='email' name='email' placeholder='Your Email' required/>
+          <textarea name='message' rows='7' placeholder='Your Message' required/>
+          <button type='submit' className='btn btn-primary'>Submit Form</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
