1a5f5534695d011dc69350debcbeeb16ff2c0b29
MariosKampouris <marioswow20@gmail.com>
2023-01-26 01:10:09 +0200

    Created Contact Section and implemented EmailJS in the project

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/md'
-import { BsWhatsapp } from 'react-icons/bs'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ygywjye', 'template_qqp5xkq', form.current, 'XYXJihG1gzsl3slkU')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -13,26 +30,26 @@ const Contact = () => {
       <div className='container contact__container'>
         <div className='contact__options'>
           <article className='contact__option'>
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>marioswow20@gmail.com</h5>
             <a href='mailto:marioswow20@gmail.com' target='_blank' rel='noreferrer'>Send a Message</a>
           </article>
           <article className='contact__option'>
-            <MdOutlineEmail/>
+            <RiMessengerLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>dummymessenger</h5>
             <a href='mailto:dummyegator@gmail.com' target='_blank' rel='noreferrer'>Send a Message</a>
           </article>
           <article className='contact__option'>
-            <MdOutlineEmail/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+153113132</h5>
             <a href='mailto:dummyegator@gmail.com' target='_blank' rel='noreferrer'>Send a Message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your Full Name' required/>
           <input type='email' name='email' placeholder='Your Email' required/>
           <textarea name='message' rows='7' placeholder='Your Message' required/>
