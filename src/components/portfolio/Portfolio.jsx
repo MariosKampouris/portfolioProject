04094d81eeef88d93ed4ec38efac58a9513341d5
MariosKampouris <marioswow20@gmail.com>
2023-02-02 21:49:20 +0200

    Create new Portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useRef, useState } from "react"
 import './portfolio.css'
 import IMG1 from '../../assets/swordofkings.JPG'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation} from "swiper";
+import redapple from '../../assets/redapple.JPG'
 
 function Portfolio() {
   return (
@@ -8,68 +14,51 @@ function Portfolio() {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='img1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn'>Github</a>
-            <a href='https://dribbble.com/search/alien-pixels' className='btn btn-primary' target={'_blank'} rel='noreferrer'>Live Demo</a>    
-          </div>      
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='img1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn'>Github</a>
-            <a href='https://dribbble.com/search/alien-pixels' className='btn btn-primary' target={'_blank'} rel='noreferrer'>Live Demo</a>    
-          </div>        
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='img1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn'>Github</a>
-            <a href='https://dribbble.com/search/alien-pixels' className='btn btn-primary' target={'_blank'} rel='noreferrer'>Live Demo</a>    
-          </div>         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='img1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn'>Github</a>
-            <a href='https://dribbble.com/search/alien-pixels' className='btn btn-primary' target={'_blank'} rel='noreferrer'>Live Demo</a>    
-          </div>         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='img1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn'>Github</a>
-            <a href='https://dribbble.com/search/alien-pixels' className='btn btn-primary' target={'_blank'} rel='noreferrer'>Live Demo</a>    
-          </div>         
-        </article>
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt='img1'/>
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className='portfolio__item-cta'>
-            <a href='https://github.com' className='btn'>Github</a>
-            <a href='https://dribbble.com/search/alien-pixels' className='btn btn-primary' target={'_blank'} rel='noreferrer'>Live Demo</a>    
-          </div>       
-        </article>
-      </div>
+      <Swiper
+        className="portfolio__Swiper"
+        navigation={true}
+        centeredSlides={true}
+        pagination={{
+          dynamicMainBullets: true,
+          clickable: true
+        }}
+        modules={[Pagination, Navigation]}
+        spaceBetween={20}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+          <SwiperSlide>
+            <div className="portfolio__item"> 
+              <div className="portfolio__image__container">
+                <div className="portfolio__Swiper__Image">
+                  <img src={redapple} alt='About Image'/>
+                </div>
+                <div className="portfolio__Swiper__Image">
+                  <img src={redapple} alt='About Image'/>
+                </div>
+                <div className="portfolio__Swiper__Image">
+                  <img src={redapple} alt='About Image'/>
+                </div>
+              </div>
+              <h3>Unity 3D RPG Game Project</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut egestas ac risus a pellentesque. 
+                Nulla eget neque arcu. Nulla non gravida elit. Duis et facilisis ligula. 
+                Fusce nec magna vel magna posuere aliquam id sit amet magna.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            slide 2
+          </SwiperSlide>
+          <SwiperSlide>
+            slide 3
+          </SwiperSlide>
+          <SwiperSlide>
+            slide 4
+          </SwiperSlide>
+      </Swiper>
     </section>
   )
 }
